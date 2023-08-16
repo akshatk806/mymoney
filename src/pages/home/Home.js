2748fbf80813c2ec5ec95c62ff8f9cd891aeac2e
akshatk806 <akshatk806@gmail.com>
2023-08-16 13:41:26 +0530

    021 Transaction form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,18 @@
 import styles from './Home.module.css'
 
+import TransactionForm from './TransactionForm'
+
 const Home = () => {
   return (
-    <div>
-        Home
+    <div className={styles.container}>
+        <div className={styles.content}>
+          transaction list
+        </div>
+
+        <div className={styles.sidebar}>
+          {/* transaction form */}
+          <TransactionForm />
+        </div>
     </div>
   )
 }
