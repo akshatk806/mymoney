7f1fccedb883de4c9fa81405894e375bae27b97c
akshatk806 <akshatk806@gmail.com>
2023-08-16 12:48:36 +0530

    019 Solve the flikering of buttons on navbar problem

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import './App.css'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+// hooks
+import { useAuthContext } from './hooks/useAuthContext';
+
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar'
 
 function App() {
+  const { authIsReady } = useAuthContext()
+  // now we don't see that flickering on navbar
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
